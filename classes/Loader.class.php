b99e42bc032e4d3945ff26dd4b1d2c01f437a0a1
gsarig <contact@gsarigiannidis.gr>
2024-03-01 14:38:39 +0200

    Fixes a regression caused by the previous update

--- classes/Loader.class.php
+++ classes/Loader.class.php
@@ -70,12 +70,13 @@ class Loader {
 		$styles = '#slash_admin_loader-container{position:fixed;z-index:99999;width:100%;height:100%;background:#fff;top:0;overflow:hidden;opacity:0;pointer-events:none;transition:opacity .6s}#slash_admin_loader-container.loading{opacity:1;pointer-events:all}#slash_admin_loader-container .slash_admin_logo-loader{opacity:1;top:50%;left:calc(50% - 48px);transform:translateY(-50%);position:absolute;width:96px;height:96px}#slash_admin_loader-container .slash_admin_logo-loader img{height:auto}.slash_admin_image-rounded img{border-radius:50%}.slash_admin_loader_delay-msg{position:absolute;z-index:99999999;width:100%;text-align:center;bottom:10%}#slash_admin_loader-container .slash_admin_loader{margin:-5em auto 0 auto;font-size:10px;text-indent:-9999em;border-top:.2em solid #ccc;border-right:.2em solid #ccc;border-bottom:.2em solid #ccc;border-left:.2em solid #000;position:absolute;top:50%;left:calc(50% - 5em);transform:translate(-50%, 0);animation:load8 1.1s infinite linear}#slash_admin_loader-container .slash_admin_loader,#slash_admin_loader-container .slash_admin_loader:after{border-radius:50%;width:10em;height:10em}@-webkit-keyframes load8{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}@keyframes load8{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}';
 		if ( self::hasCustomColors() ) {
 			// Custom styles
-			$background    = Settings::option( 'loading_bg_color' );
-			$empty_spinner = Settings::option( 'loading_spinner_empty' );
-			$full_spinner  = Settings::option( 'loading_spinner_full' );
+			$background    = safecss_filter_attr( Settings::option( 'loading_bg_color' ) );
+			$empty_spinner = safecss_filter_attr( Settings::option( 'loading_spinner_empty' ) );
+			$full_spinner  = safecss_filter_attr( Settings::option( 'loading_spinner_full' ) );
 			$styles        .= "#slash_admin_loader-container {background: {$background};}.slash_admin_loader_delay-msg {color: {$full_spinner};}.slash_admin_loader {border-top-color: {$empty_spinner};border-right-color: {$empty_spinner};border-bottom-color: {$empty_spinner};border-left-color: {$full_spinner};}";
 		}
-	 		echo $styles ? '<style>' . safecss_filter_attr( $styles ) . '</style>' : '';
+
+		echo $styles ? '<style>' . $styles . '</style>' : '';
 	}
 
 	private static function image( $alt_text ) {
